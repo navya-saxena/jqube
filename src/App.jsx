53fded7ffabd16d5f88199f0a4331a8b841d3dc5
navya-saxena <navyasaxena.6422@gmail.com>
2024-10-15 19:53:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Journals from './components/Journals'; // Import your Journals component
-import Conferences from './components/Conferences';
-
-
 import Home from './pages/Home';
 
 import './App.css';
@@ -16,8 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/journals" component={Journals} />
-        <Route path="/conferences" component={Conferences} />
+    
       </Routes>
       <Footer />
     </Router>
