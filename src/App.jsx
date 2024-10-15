c9fba4e642867be5ace2a63bfaf63e0f6d738fae
sejaljain244 <sj244nmh@gmail.com>
2024-10-16 01:03:11 +0530

    homepage updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
